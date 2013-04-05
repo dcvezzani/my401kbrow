a78856105ec6265d57408da757a23668683d2af7
David Vezzani <david_vezzani@yahoo.com>
2013-04-05 09:38:51 -0700

    corrects label for seed create, removes 'here i am' and adjusts for some weirdness with twitter bootstrap; need to figure this out later

--- db/my401k.seeds.rb
+++ db/my401k.seeds.rb
@@ -2,7 +2,7 @@ require 'cms/data_loader'
 extend Cms::DataLoader
 
 if(Cms::Category.count == 0)
-  my401k_product_type = create_category_type(:guest, :name => 'My401k Product')
+  my401k_product_type = create_category_type(:my401k_product, :name => 'My401k Product')
 
   create_category(:about_plan, :name => 'About Plan', :category_type => my401k_product_type)
   create_category(:manage_account, :name => 'Manage Account', :category_type => my401k_product_type)
