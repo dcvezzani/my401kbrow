50911b9721d276d60a01cd798686b65fdfba239c
Michael Vezzani <mjvezzani@gmail.com>
2013-05-30 16:28:23 -0400

    Gemfile and Schema

--- db/schema.rb
+++ db/schema.rb
@@ -500,60 +500,6 @@ ActiveRecord::Schema.define(:version => 20130402092925) do
     t.integer  "latest_version"
   end
 
-  create_table "my401k_article_versions", :force => true do |t|
-    t.integer  "original_record_id"
-    t.integer  "version"
-    t.integer  "section_id_id"
-    t.integer  "layout_id_id"
-    t.text     "sub_title"
-    t.text     "body"
-    t.text     "summary"
-    t.text     "tile_title"
-    t.text     "tile_icon"
-    t.datetime "created_at",                            :null => false
-    t.datetime "updated_at",                            :null => false
-    t.string   "name"
-    t.boolean  "published",          :default => false
-    t.boolean  "deleted",            :default => false
-    t.boolean  "archived",           :default => false
-    t.string   "version_comment"
-    t.integer  "created_by_id"
-    t.integer  "updated_by_id"
-  end
-
-  create_table "my401k_meeting_versions", :force => true do |t|
-    t.integer  "original_record_id"
-    t.integer  "version"
-    t.string   "name"
-    t.boolean  "active"
-    t.datetime "start_date"
-    t.datetime "end_time"
-    t.datetime "created_at",                            :null => false
-    t.datetime "updated_at",                            :null => false
-    t.boolean  "published",          :default => false
-    t.boolean  "deleted",            :default => false
-    t.boolean  "archived",           :default => false
-    t.string   "version_comment"
-    t.integer  "created_by_id"
-    t.integer  "updated_by_id"
-  end
-
-  create_table "my401k_meetings", :force => true do |t|
-    t.integer  "version"
-    t.integer  "lock_version",  :default => 0
-    t.string   "name"
-    t.boolean  "active"
-    t.datetime "start_date"
-    t.datetime "end_time"
-    t.datetime "created_at",                       :null => false
-    t.datetime "updated_at",                       :null => false
-    t.boolean  "published",     :default => false
-    t.boolean  "deleted",       :default => false
-    t.boolean  "archived",      :default => false
-    t.integer  "created_by_id"
-    t.integer  "updated_by_id"
-  end
-
   create_table "page_route_options", :force => true do |t|
     t.integer  "page_route_id"
     t.string   "type"
